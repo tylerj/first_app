45d32183dd0b7695f98c37c706e5460aaf1f68e1
Tyler Johnson <tylerl.johnson@gmail.com>
2011-07-27 14:02:38 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicksLeague::Application.config.secret_token = 'd25b16b488bda97ded42d8facb93c55108975673f6f5211dc20792069cb94fa9401f0f2e0a8a0905a5d4672325150dfe18426c6676f75e1fb245fe00e507263e'
