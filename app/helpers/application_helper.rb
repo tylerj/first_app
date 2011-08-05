9c6bfc98443a185cbb7ac6867d4101d6c8ff503d
Tyler Johnson <tylerl.johnson@gmail.com>
2011-08-05 12:53:25 -0400

    Updated the logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def logo
     # Fill in.
-    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+    logo = image_tag("logo.jpg", :alt => "PicksLeague.com", :class => "round logo")
   end  
   # Return a title on a per-page basis.
   def title
