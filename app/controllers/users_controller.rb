39593df85ae40646db1120deea03c526c45f6116
Tyler Johnson <tylerl.johnson@gmail.com>
2011-08-08 10:07:32 -0400

    Finish sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,37 @@ class UsersController < ApplicationController
   end
   
   def new
+    redirect_to root_path if signed_in?
+    @user = User.new
     @title = "Sign up"
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to Picks League!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      @user.password = nil
+      @user.password_confirmation = nil
+      render 'new'
+    end
+  end
 
+  private
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def signed_in_user
+      redirect_to(root_path) if signed_in?
+    end
 end
